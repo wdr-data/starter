46e52b55b8e69247e6d93e8dbf36cf04771b6199
Christine <christine.gotth@gmail.com>
2019-06-26 17:00:05 +0200

    🔧 Update config file

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,9 +1,19 @@
-import { configure } from '@storybook/react';
+import React from 'react';
+import { configure, addDecorator } from '@storybook/react';
+import '../src/css/colors.css';
 
 const req = require.context('../src/components', true, /\.stories\.jsx?$/);
+
+const BackgroundDecorator = (story) => (
+  <div style={{backgroundColor: 'var(--storybook-background)', height: '100vh'}}>
+    {story()}
+  </div>
+);
+
+addDecorator(BackgroundDecorator);
 
 function loadStories() {
     req.keys().forEach(filename => req(filename));
   }
 
- configure(loadStories, module);
+configure(loadStories, module);
