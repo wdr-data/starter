167ed7371b2e49238224d6eec91a4dace2c18014
Marcus Weiner <marcus.weiner@gmail.com>
2019-06-28 11:57:58 +0200

    🔧 Fix css module loading in breadcrumbs

--- src/components/breadcrumbs/breadcrumbs.jsx
+++ src/components/breadcrumbs/breadcrumbs.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import styles from './breadcrumbs.css';
+import styles from './breadcrumbs.module.css';
 
 const Breadcrumbs = ({ children }) => {
 
