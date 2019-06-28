3aefd82b1170d81f3232ce3b820e160705e922c9
Marcus Weiner <marcus.weiner@gmail.com>
2019-06-28 12:39:16 +0200

    💄 Use proper arrow in breadcrumbs

--- .storybook/webpack.config.js
+++ .storybook/webpack.config.js
@@ -1,24 +1,26 @@
 module.exports = {
-    module: {
-      rules: [
-        {
-          test: /\.css$/,
-          loaders: [
-            require.resolve('style-loader'),
-            {
-              loader: require.resolve('css-loader'),
-              options: {
-                importLoaders: 1,
-                modules: true,
-                localsConvention: 'camelCase',
-              },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        loaders: [
+          require.resolve('style-loader'),
+          {
+            loader: require.resolve('css-loader'),
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localsConvention: 'camelCase',
             },
-          ],
-        },
-        {
-          test: /\.(jpg|png|gif)$/,
-          loader: require.resolve('file-loader'),
-        },
-      ],
-    },
-  }
+          },
+        ],
+      },
+      // Following config is based on the Webpack config from Gatsby
+      // to archieve parity
+      {
+        test: /\.(ico|svg|jpg|jpeg|png|gif|webp)(\?.*)?$/,
+        loader: require.resolve('url-loader'),
+      },
+    ],
+  },
+}
