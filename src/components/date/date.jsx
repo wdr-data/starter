2b4f26585e4416b8c22f9612b4dad37ddc384510
Marcus Weiner <marcus.weiner@gmail.com>
2019-06-28 11:55:00 +0200

    🔧 Fix css module loading for gatsby

--- src/components/date/date.jsx
+++ src/components/date/date.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-import styles from './date.css';
+import styles from './date.module.css';
 
 const DateFormat = ({ date }) => {
     return <span className={styles.date}>{date.toLocaleDateString('de-DE', {year: 'numeric', month: 'long', day: 'numeric' })}</span>
