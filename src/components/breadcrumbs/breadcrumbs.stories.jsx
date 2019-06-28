0d56008e9915d87e8d10277d1d814f91f93a26bf
Christine <christine.gotth@gmail.com>
2019-06-28 11:04:49 +0200

    ✨ Add breadcrumbs component

---
+++ src/components/breadcrumbs/breadcrumbs.stories.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import Breadcrumbs from './breadcrumbs.jsx';
+
+storiesOf('Breadcrumbs', module).add('default', () => (
+    <Breadcrumbs>
+        <a>WDR</a>
+        <a>Nachrichten</a>
+        <a>Landespolitik</a>
+        <a>aktuelle Seite</a>
+    </Breadcrumbs>
+))
