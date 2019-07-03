6c63546f810e848c19fa714d2db189c69a92900b
Christine <christine.gotth@gmail.com>
2019-07-03 10:45:21 +0200

    🔒 Add rel=noopener to a tags

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -6,13 +6,13 @@ const Footer = () => {
 
     return <footer className={styles.footer}>
             <nav className={styles.links}>
-                <a href='https://www1.wdr.de/hilfe/index.html' target='_blank'>Hilfe</a>
-                <a href='https://www1.wdr.de/impressum/index.html' target='_blank'>Impressum</a>
-                <a href='https://www1.wdr.de/hilfe/datenschutz102.html' target='_blank'>Datenschutz</a>
-                <a href='https://www1.wdr.de/kontakt/index.html' target='_blank'>Kontakt</a>
+                <a href='https://www1.wdr.de/hilfe/index.html' target='_blank' rel='noopener'>Hilfe</a>
+                <a href='https://www1.wdr.de/impressum/index.html' target='_blank' rel='noopener'>Impressum</a>
+                <a href='https://www1.wdr.de/hilfe/datenschutz102.html' target='_blank' rel='noopener'>Datenschutz</a>
+                <a href='https://www1.wdr.de/kontakt/index.html' target='_blank' rel='noopener'>Kontakt</a>
                 <a>Bildrechte</a>
             </nav>
-            <a href='https://www1.wdr.de/copyright/index.html' target='_blank' className={styles.copyrightLink}>© WDR {(new Date().getFullYear())}</a>
+            <a href='https://www1.wdr.de/copyright/index.html' target='_blank' rel='noopener' className={styles.copyrightLink}>© WDR {(new Date().getFullYear())}</a>
         </footer>
 };
 
