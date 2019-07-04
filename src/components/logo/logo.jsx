6f9ba214f886ff0053bdc35c9f0adf2049d38d84
Christine <christine.gotth@gmail.com>
2019-07-04 16:05:28 +0200

    👌 Pass props in component

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,15 +1,25 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import classNames from 'class-names';
 
 import styles from './logo.module.css';
 import wdrLogo from '!svg-inline-loader!./logo_wdr.svg';
 
-const Logo = () => {
+const Logo = ({
+        className,
+        ...props,
+    }) => {
     return <a href={'https://www1.wdr.de/index.html'} target='_blank' rel='noopener' title='zur WDR Startseite' aria-label='zur WDR Startseite'>
         <div
-            className={styles.logo}
+            className={classNames(styles.logo, className)}
             dangerouslySetInnerHTML={{__html: wdrLogo}}
+            {...props}
         />
     </a>;
+};
+
+Logo.propTypes = {
+    className: PropTypes.string,
 };
 
 export default Logo;
