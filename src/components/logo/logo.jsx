8acdcd5579e37c4701133eeb4589c0144ec0605a
Christine <christine.gotth@gmail.com>
2019-07-04 12:25:30 +0200

    ♿️ Convert logo into proper link

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,25 +1,15 @@
-import PropTypes from 'prop-types';
 import React from 'react';
-import classNames from 'class-names';
 
 import styles from './logo.module.css';
 import wdrLogo from '!svg-inline-loader!./logo_wdr.svg';
 
-const Logo = ({
-    className,
-    onClick,
-    ...props
-}) => {
-    return <div
-        className={classNames(styles.logo, className)}
-        onClick={onClick}
-        {...props}
-        dangerouslySetInnerHTML={{__html: wdrLogo}}/>;
-};
-
-Logo.propTypes = {
-    className: PropTypes.string,
-    onClick: PropTypes.func,
+const Logo = () => {
+    return <a href={'https://www1.wdr.de/index.html'} target='_blank' rel='noopener' title='zur WDR Startseite' aria-label='zur WDR Startseite'>
+        <div
+            className={styles.logo}
+            dangerouslySetInnerHTML={{__html: wdrLogo}}
+        />
+    </a>;
 };
 
 export default Logo;
