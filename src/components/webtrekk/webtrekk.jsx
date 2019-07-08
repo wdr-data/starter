77e1a3575fd67cc700e068a2973ebe066e097be2
P3nny <p3nny@hacking.studio>
2019-07-08 14:55:04 +0200

    ✨ Add webtrekk tracking component

---
+++ src/components/webtrekk/webtrekk.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+
+const Webtrekk = () => {
+    return <img style={{display: "none"}}
+    src="https://wdr01.wt-eu02.net/882049745744921/wt?p=441,WDR_Verbraucher_Digital_Data_Daten sichtbar machen,1,1920x1200,24,1,1562577868831,0,975x1023,0&tz=2&la=de&cg1=WDR&cg2=Verbraucher&cg3=Digital&cg4=Data&cg5=Daten sichtbar machen&cp4=2019-07-06&np=Shockwave%20Flash&pu=https%3A%2F%2Fwww1.wdr.de%2Fverbraucher%2Fdigital%2Fdata%2Findex.html&eor=1"/>
+}
+
+export default Webtrekk;
