7483f58367d20dae8bd815388593584e849b0894
Christine <christine.gotth@gmail.com>
2019-07-08 11:06:52 +0200

    📱 Make header responsive

--- src/components/logo/logo.jsx
+++ src/components/logo/logo.jsx
@@ -1,34 +1,28 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import classNames from 'class-names';
+import React from "react";
+import PropTypes from "prop-types";
+import classNames from "class-names";
 
-import styles from './logo.module.css';
-import wdrLogo from '!svg-inline-loader!./logo_wdr.svg';
+import styles from "./logo.module.css";
+import wdrLogo from "!svg-inline-loader!./logo_wdr.svg";
 
-const Logo = ({
-        className,
-        href,
-        title,
-        ...props,
-    }) => {
-    return <a
-            href={href || 'https://www1.wdr.de/index.html'}
-            title={title || 'zur WDR Startseite'}
-            target='_blank'
-            rel='noopener'
-            {...props}
-        >
-        <span
-            className={classNames(styles.logo, className)}
-            dangerouslySetInnerHTML={{__html: wdrLogo}}
-        />
-    </a>;
+const Logo = ({ className, href, title, ...props }) => {
+  return (
+    <a
+      href={href || "https://www1.wdr.de/index.html"}
+      title={title || "zur WDR Startseite"}
+      target="_blank"
+      rel="noopener"
+      {...props}
+    >
+      <span className={classNames(styles.logo, className)} dangerouslySetInnerHTML={{ __html: wdrLogo }} />
+    </a>
+  );
 };
 
 Logo.propTypes = {
-    className: PropTypes.string,
-    href: PropTypes.string,
-    title: PropTypes.string,
+  className: PropTypes.string,
+  href: PropTypes.string,
+  title: PropTypes.string
 };
 
 export default Logo;
