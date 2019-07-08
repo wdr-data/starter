33c3c9706f4c38ee80250e8ce5f929e25084053c
P3nny <p3nny@hacking.studio>
2019-07-08 15:37:57 +0200

    🎨 Refactor url params

--- src/components/webtrekk/webtrekk.jsx
+++ src/components/webtrekk/webtrekk.jsx
@@ -1,8 +1,24 @@
 import React from "react";
 
-const Webtrekk = () => {
-    return <img style={{display: "none"}}
-    src="https://wdr01.wt-eu02.net/882049745744921/wt?p=441,WDR_Verbraucher_Digital_Data_Daten sichtbar machen,1,1920x1200,24,1,1562577868831,0,975x1023,0&tz=2&la=de&cg1=WDR&cg2=Verbraucher&cg3=Digital&cg4=Data&cg5=Daten sichtbar machen&cp4=2019-07-06&np=Shockwave%20Flash&pu=https%3A%2F%2Fwww1.wdr.de%2Fverbraucher%2Fdigital%2Fdata%2Findex.html&eor=1"/>
+
+const Webtrekk = ({ cg1, cg2, cg3, cg4, cg5 }) => {
+    const searchParams = new URLSearchParams();
+    searchParams.append('p', '441,WDR_Verbraucher_Digital_Data_Daten sichtbar machen,1,1920x1200,24,1,1562577868831,0,975x1023,0') /*TODO Change URL?*/
+    searchParams.append('tz', '2')
+    searchParams.append('la', 'de')
+    if (cg1) { searchParams.append('cg1', cg1)}
+    if (cg2) { searchParams.append('cg2', cg2)}
+    if (cg3) { searchParams.append('cg3', cg3)}
+    if (cg4) { searchParams.append('cg4', cg4)}
+    if (cg5) { searchParams.append('cg5', cg5)}
+    searchParams.append('cp4', '2019-07-06')
+    searchParams.append('np', 'Shockwave Flash')
+    searchParams.append('pu', 'https://www1.wdr.de/verbraucher/digital/data/index.html')
+    searchParams.append('eor', '1')
+
+    const URL = `https://wdr01.wt-eu02.net/882049745744921/wt?${searchParams.toString()}`
+
+    return <img style={{ display: "none" }} src={URL} />
 }
 
 export default Webtrekk;
