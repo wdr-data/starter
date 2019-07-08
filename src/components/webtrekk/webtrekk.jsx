0a25ca9091986533628cab2b257b388c7fb1e718
P3nny <p3nny@hacking.studio>
2019-07-08 16:19:40 +0200

    🎨 Remove obsolete Webtrekk parameters

--- src/components/webtrekk/webtrekk.jsx
+++ src/components/webtrekk/webtrekk.jsx
@@ -1,22 +1,35 @@
 import React from "react";
 
-
-const Webtrekk = ({ cg1, cg2, cg3, cg4, cg5 }) => {
+const Webtrekk = ({ cg1, cg2, cg3, cg4, cg5, publishedAt }) => {
+    const id = '882049745744921'
     const searchParams = new URLSearchParams();
-    searchParams.append('p', '441,WDR_Verbraucher_Digital_Data_Daten sichtbar machen,1,1920x1200,24,1,1562577868831,0,975x1023,0') /*TODO Change URL?*/
+    var title = []
     searchParams.append('tz', '2')
     searchParams.append('la', 'de')
-    if (cg1) { searchParams.append('cg1', cg1)}
-    if (cg2) { searchParams.append('cg2', cg2)}
-    if (cg3) { searchParams.append('cg3', cg3)}
-    if (cg4) { searchParams.append('cg4', cg4)}
-    if (cg5) { searchParams.append('cg5', cg5)}
-    searchParams.append('cp4', '2019-07-06')
-    searchParams.append('np', 'Shockwave Flash')
-    searchParams.append('pu', 'https://www1.wdr.de/verbraucher/digital/data/index.html')
-    searchParams.append('eor', '1')
+    if (cg1) {
+        searchParams.append('cg1', cg1)
+        title = title.concat(cg1)
+    }
+    if (cg2) {
+        searchParams.append('cg2', cg2)
+        title = title.concat(cg2)
+    }
+    if (cg3) {
+        searchParams.append('cg3', cg3)
+        title = title.concat(cg3)
+    }
+    if (cg4) {
+        searchParams.append('cg4', cg4)
+        title = title.concat(cg4)
+    }
+    if (cg5) {
+        searchParams.append('cg5', cg5)
+        title = title.concat(cg5)
+    }
+    searchParams.append('cp4', publishedAt)
+    searchParams.append('p', `441,${title.join('_')},1,1920x1200,24,1,1562577868831,0,975x1023,0`)
 
-    const URL = `https://wdr01.wt-eu02.net/882049745744921/wt?${searchParams.toString()}`
+    const URL = `https://wdr01.wt-eu02.net/${id}/wt?${searchParams.toString()}`
 
     return <img style={{ display: "none" }} src={URL} />
 }
