61dc6fd60f02871d4562accd1d65faa8fa9292d1
P3nny <p3nny@hacking.studio>
2019-07-09 10:16:54 +0200

    Revert "🚧 Force redirect landing page to first article"
    This reverts commit 1f1f65242ed313010b22de36b2c2507ae63e0f5d.

--- gatsby-node.js
+++ gatsby-node.js
@@ -23,7 +23,7 @@ exports.createPages = async ({ graphql, actions }) => {
 
     result.data.allMdx.edges.forEach(({ node }) => {
         createPage({
-            path: '/',
+            path: node.frontmatter.slug,
             component: path.resolve(`./src/templates/default.jsx`),
             context: { id: node.id },
         })
