fcc41adc279b98a0c4532258c221585eb0cb4556
Christine <christine.gotth@gmail.com>
2019-07-09 10:55:47 +0200

    🎨 Add unique key for each accordion item

--- src/components/accordion/accordion.jsx
+++ src/components/accordion/accordion.jsx
@@ -1,41 +1,42 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
 import {
-    Accordion as AccordionWrapper,
-    AccordionItem,
-    AccordionItemHeading,
-    AccordionItemButton,
-    AccordionItemPanel,
-} from 'react-accessible-accordion';
+  Accordion as AccordionWrapper,
+  AccordionItem,
+  AccordionItemHeading,
+  AccordionItemButton,
+  AccordionItemPanel
+} from "react-accessible-accordion";
 
-import styles from './accordion.module.css';
+import styles from "./accordion.module.css";
 
 const Accordion = ({ authors, sources, credits }) => {
-    return (
-        <AccordionWrapper className={styles.wrapper} allowZeroExpanded>
-            {[["Autor*innen", authors], ["Quellen", sources], ["Credits", credits]].map(([title, content]) => content && (
-                <AccordionItem className={styles.item}>
-                    <AccordionItemHeading className={styles.heading}>
-                        <AccordionItemButton className={styles.button}>
-                            <h4>{title}</h4>
-                        </AccordionItemButton>
-                    </AccordionItemHeading>
-                    <AccordionItemPanel className={styles.panel}>
-                        {content}
-                    </AccordionItemPanel>
-                </AccordionItem>
-            ))}
-        </AccordionWrapper>
-    );
-}
+  return (
+    <AccordionWrapper className={styles.wrapper} allowZeroExpanded>
+      {[["Autor*innen", authors], ["Quellen", sources], ["Credits", credits]].map(
+        ([title, content]) =>
+          content && (
+            <AccordionItem className={styles.item} key={title}>
+              <AccordionItemHeading className={styles.heading}>
+                <AccordionItemButton className={styles.button}>
+                  <h4>{title}</h4>
+                </AccordionItemButton>
+              </AccordionItemHeading>
+              <AccordionItemPanel className={styles.panel}>{content}</AccordionItemPanel>
+            </AccordionItem>
+          )
+      )}
+    </AccordionWrapper>
+  );
+};
 
 const nodePropType = PropTypes.oneOfType(PropTypes.node, PropTypes.arrayOf(PropTypes.node));
 
 Accordion.propTypes = {
-    authors: nodePropType,
-    sources: nodePropType,
-    credits: nodePropType,
+  authors: nodePropType,
+  sources: nodePropType,
+  credits: nodePropType
 };
 
 export default Accordion;
