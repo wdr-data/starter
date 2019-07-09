3f0103c171c30abd88c1a1f2c696f0f655b1e0c6
Christine <christine.gotth@gmail.com>
2019-07-09 10:53:29 +0200

    🎨 Fix prop type validation

--- src/components/quote/quote.jsx
+++ src/components/quote/quote.jsx
@@ -1,25 +1,20 @@
-import React from 'react';
-import PropTypes from 'prop-types';
+import React from "react";
+import PropTypes from "prop-types";
 
-import styles from './quote.module.css';
+import styles from "./quote.module.css";
 
-const Quote = ({
-    children,
-    author,
-  }) => {
-    return (
-        <blockquote className={styles.wrapper}>
-            <p className={styles.quote}>{children}</p>
-            <cite className={styles.author}>
-                {author}
-            </cite>
-        </blockquote>
-    )
+const Quote = ({ children, author }) => {
+  return (
+    <blockquote className={styles.wrapper}>
+      <p className={styles.quote}>{children}</p>
+      <cite className={styles.author}>{author}</cite>
+    </blockquote>
+  );
 };
 
 Quote.propTypes = {
-    children: PropTypes.string,
-    author: PropTypes.string,
+  children: PropTypes.string,
+  author: PropTypes.node
 };
 
 export default Quote;
