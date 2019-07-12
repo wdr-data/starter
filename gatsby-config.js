ddcb3e7f427f307ecaf73bcfd30e2b9b7da5347c
Christine <christine.gotth@gmail.com>
2019-07-12 14:11:41 +0200

    ➕ Include gatsby styled components in build

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,23 +3,24 @@ module.exports = {
   plugins: [
     `gatsby-plugin-react-helmet`,
     `gatsby-plugin-sharp`,
+    `gatsby-plugin-styled-components`,
     {
       resolve: `gatsby-mdx`,
       options: {
         extensions: [`.mdx`, `.md`],
         defaultLayouts: {
-          default: require.resolve("./src/templates/default.jsx"),
+          default: require.resolve("./src/templates/default.jsx")
         },
         gatsbyRemarkPlugins: [
           {
             resolve: `gatsby-remark-images`,
             options: {
               maxWidth: 1035,
-              sizeByPixelDensity: true,
-            },
-          },
-        ],
-      },
-    },
-  ],
-}
+              sizeByPixelDensity: true
+            }
+          }
+        ]
+      }
+    }
+  ]
+};
