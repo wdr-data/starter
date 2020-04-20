ae0e840df54bd8de8a4d57da2ecd5743e268a9f7
Jannes Höke <mail@jhoeke.de>
2020-04-20 10:36:16 +0200

    🚨 Use `className` attribute

--- src/components/ydi/ydiWrapper.jsx
+++ src/components/ydi/ydiWrapper.jsx
@@ -9,24 +9,24 @@ export const YDIWrapper = ({ question, confirmAllowed, onConfirm, children }) =>
         onConfirm();
     })
     return (
-        <div class="questions">
-            <div class="question">
+        <div className="questions">
+            <div className="question">
                 <h2>{question.heading}</h2>
                 <h3>{question.subheading}</h3>
-                <div class={`you-draw-it ${question.key}`} data-key={question.key} aria-labelledby={`hint-${question.key}`}>
+                <div className={`you-draw-it ${question.key}`} data-key={question.key} aria-labelledby={`hint-${question.key}`}>
                     {children}
                 </div>
-                <div id={`hint-${question.key}`} class="accessibility" hidden aria-hidden="false">
+                <div id={`hint-${question.key}`} className="accessibility" hidden aria-hidden="false">
                     {question.accessibility}
                 </div>
-                <div class={`result ${question.key}`}>
-                    <div class="actionContainer" aria-hidden="true">
-                        <button class="showAction" disabled={!confirmAllowed} onClick={confirmHandler}>Wie ist es tatsächlich?</button>
-                        <div class="tooltipcontainer">
-                            <span class="tooltiptext">Ziehen Sie den Balken! Der Klick verrät, ob ihre Schätzung stimmt.</span>
+                <div className={`result ${question.key}`}>
+                    <div className="actionContainer" aria-hidden="true">
+                        <button className="showAction" disabled={!confirmAllowed} onClick={confirmHandler}>Wie ist es tatsächlich?</button>
+                        <div className="tooltipcontainer">
+                            <span className="tooltiptext">Ziehen Sie den Balken! Der Klick verrät, ob ihre Schätzung stimmt.</span>
                         </div>
                     </div>
-                    <div class="text" hidden={!confirmed} aria-hidden="false">{question.result}</div>
+                    <div className="text" hidden={!confirmed} aria-hidden="false">{question.result}</div>
                 </div>
             </div>
         </div>
