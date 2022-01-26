d3d463ea24637ac2703a987d333ac8d9e8ab5680
Jannes Höke <mail@jhoeke.de>
2022-01-26 21:06:47 +0100

    🔥 Remove some old code

--- src/components/quiz/quiz.jsx
+++ src/components/quiz/quiz.jsx
@@ -4,8 +4,6 @@ import classNames from 'class-names'
 import ReactMarkdown from 'react-markdown'
 import { v4 as uuid } from 'uuid'
 
-import Webtrekk from '../webtrekk/webtrekk'
-import FrontmatterContext from '../../templates/frontmatterContext'
 import styles from './quiz.module.css'
 
 const QuizContext = React.createContext({
@@ -111,91 +109,6 @@ export const Result = ({ children }) => {
       </div>
       <div className={styles.text} hidden={!confirmed} aria-hidden={confirmed ? "false" : "true"}>
         <ReactMarkdown source={children} linkTarget="_blank" />
-      </div>
-    </div>
-  )
-}
-
-export const Foo = ({ question, confirmAllowed, onConfirm, children }) => {
-  const [confirmed, setConfirmed] = useState(false)
-  const confirmHandler = useCallback(() => {
-    setConfirmed(true)
-    onConfirm()
-  }, [setConfirmed, onConfirm])
-  return (
-    <div className={styles.questions}>
-      <div className={styles.question}>
-        <h4>{question.heading}</h4>
-        <figcaption>{question.subheading}</figcaption>
-        <div
-          className={classNames(styles.youDrawIt)}
-          aria-labelledby={`hint-${question.key}`}
-        >
-          {children}
-        </div>
-        <div
-          id={`hint-${question.key}`}
-          className={styles.accessibility}
-          hidden
-          aria-hidden="false"
-        >
-          {question.accessibility}
-        </div>
-        <div
-          className={classNames(
-            styles.result,
-            confirmAllowed && styles.finished,
-            confirmed && styles.shown,
-          )}
-        >
-          <div className={styles.actionContainer} aria-hidden="true">
-            <button
-              className={styles.showAction}
-              disabled={!confirmAllowed || confirmed}
-              onClick={confirmHandler}
-            >
-              Wie ist es tatsächlich?
-            </button>
-          </div>
-          <div className={styles.text} hidden={!confirmed} aria-hidden="false">
-            <ReactMarkdown source={question.result} linkTarget="_blank" />
-          </div>
-          <div
-            className={styles.source}
-            hidden={!confirmed}
-            aria-hidden="false"
-          >
-            <span id={`source-label-${question.key}`}>Quelle:</span>&nbsp;
-            <a
-              href={question.source.url}
-              rel="noopener noreferrer"
-              target="_blank"
-              aria-labelledby={`source-label-${question.key}`}
-            >
-              {question.source.title}
-            </a>
-          </div>
-        </div>
-
-        {confirmed && (
-          <FrontmatterContext.Consumer>
-            {(frontmatter) => {
-              const cgParams = Object.keys(frontmatter).filter((param) =>
-                param.startsWith('cg'),
-              )
-              const additionalCgParams = {
-                [`cg${cgParams.length + 1}`]: question.key,
-              }
-              return (
-                <Webtrekk
-                  {...frontmatter}
-                  publishedAt={frontmatter.pub_date}
-                  {...additionalCgParams}
-                />
-              )
-            }}
-          </FrontmatterContext.Consumer>
-        )}
       </div>
     </div>
   )
